bf7aebfdd6871c6522ae22758053dc1311dd1d32
Rihards420 <rihards.sturitis3@gmail.com>
2024-01-23 03:31:42 +0200

    form3 idejiska formatesanas izveide

--- WindowsFormsApp1/Form3.Designer.cs
+++ WindowsFormsApp1/Form3.Designer.cs
@@ -28,12 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Soli_text = new System.Windows.Forms.Label();
+            this.Minutes_text = new System.Windows.Forms.Label();
+            this.Kalorijas_text = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Soli_text
+            // 
+            this.Soli_text.AutoSize = true;
+            this.Soli_text.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Soli_text.Location = new System.Drawing.Point(40, 43);
+            this.Soli_text.Name = "Soli_text";
+            this.Soli_text.Size = new System.Drawing.Size(37, 23);
+            this.Soli_text.TabIndex = 0;
+            this.Soli_text.Text = "Soli";
+            // 
+            // Minutes_text
+            // 
+            this.Minutes_text.AutoSize = true;
+            this.Minutes_text.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minutes_text.Location = new System.Drawing.Point(40, 70);
+            this.Minutes_text.Name = "Minutes_text";
+            this.Minutes_text.Size = new System.Drawing.Size(73, 23);
+            this.Minutes_text.TabIndex = 1;
+            this.Minutes_text.Text = "Minutes";
+            // 
+            // Kalorijas_text
+            // 
+            this.Kalorijas_text.AutoSize = true;
+            this.Kalorijas_text.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Kalorijas_text.Location = new System.Drawing.Point(40, 99);
+            this.Kalorijas_text.Name = "Kalorijas_text";
+            this.Kalorijas_text.Size = new System.Drawing.Size(76, 23);
+            this.Kalorijas_text.TabIndex = 2;
+            this.Kalorijas_text.Text = "Kalorijas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "/Sasniedzamiais_skaits";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soli";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Partika";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(43, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Ievadīt kalorijas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 585);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Miegs";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 464);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Udens";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(44, 490);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ievadit udens daudzumu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(230, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Aktivitates";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(137, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 23);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Solu_skaits";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(137, 99);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(158, 23);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Kaloriju_daudzums";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(137, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(121, 23);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Minusu_skaits";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(56, 274);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(107, 83);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Staigasana";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(282, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 83);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Peldesana";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(169, 274);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 83);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Skriesana";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(395, 274);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 83);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Svaru celsana";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(40, 683);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 23);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Ieteikumi";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(39, 789);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 23);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Izdevumi";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(440, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 32);
+            this.button7.TabIndex = 22;
+            this.button7.Text = "Iestatijumi";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(564, 912);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Kalorijas_text);
+            this.Controls.Add(this.Minutes_text);
+            this.Controls.Add(this.Soli_text);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Soli_text;
+        private System.Windows.Forms.Label Minutes_text;
+        private System.Windows.Forms.Label Kalorijas_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button7;
     }
 }
